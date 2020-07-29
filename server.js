9a94688ed9933465c34f60baa1eb08dd5b234dc0
art <arthurvat7@gmail.com>
2020-07-29 14:23:35 -0700

    change procfile

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 var PORT = process.env.PORT || 5000;
+const express = require('express');
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, '/')));
 var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
