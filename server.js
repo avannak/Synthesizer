29aa51ab2512e62541d44fda864e788a6f98627d
art <arthurvat7@gmail.com>
2020-08-02 18:29:13 -0700

    moved files

--- server.js
+++ server.js
@@ -1,11 +1,3 @@
-var PORT = process.env.PORT || 5000;
-const express = require('express');
-const path = require('path');
+const name = "mario";
 
-app.use(express.static(path.join(__dirname, '/')));
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(1337, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:1337/');
+console.log(name);
